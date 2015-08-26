d6b650c39276b0bff57f64be2fea8d73d1d069a4
ravindrarjpt9 <ravindra.rajpoot@hotmail.com>
2015-08-27 00:37:56 +0530

    entity @

--- src/java/com/skt/web/alpha/model/UserPushMessage.java
+++ src/java/com/skt/web/alpha/model/UserPushMessage.java
@@ -3,12 +3,16 @@ package com.skt.web.alpha.model;
 import java.util.Date;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.Table;
 
 import com.skt.web.common.model.BaseModel;
 
+@Entity
+@Table(name = "user_push_message")
 public class UserPushMessage extends BaseModel  {
 
 	/**
